20bd1ef2b9536f6a6b7bb66cfee63b1a7c235428
Erik Seifert <erik.seifert@b-connect.de>
2017-07-03 16:56:08 +0200

    Update

---
+++ runner.sh
@@ -0,0 +1,14 @@
+if [ -z "$PLUGIN_SONAR_HOST" ]; then
+    echo "Need to set SONAR_HOST"
+    exit 0
+fi
+if [ -z "$PLUGIN_SONAR_SOURCE" ]; then
+    echo "Need to set SONAR_SOURCE"
+    exit 0
+fi
+if [ -z "$PLUGIN_SONAR_KEY" ]; then
+    echo "Need to set SONAR_KEY"
+    exit 0
+fi
+
+java -jar runner.jar -Dsonar.host.url=$PLUGIN_SONAR_HOST -Dsonar.sources=$PLUGIN_SONAR_SOURCE -Dsonar.projectKey=$PLUGIN_SONAR_KEY
